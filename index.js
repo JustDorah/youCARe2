1581e296aa805bc256afbf95f078c9e6f1384808
Dorah <dorah.bmw@gmail.com>
2019-05-03 19:04:42 -0500

    Fixed Deleted and Cleaned
    cleaned up html, js, and css
    deleted response.js
    Fixed nested td issue in table

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 "use strict";
-//https://stackoverflow.com/questions/4462478/jslint-is-suddenly-reporting-use-the-function-form-of-use-strict
 
 //I can get the maintenance schedule from here
 const maintenanceBaseURL = 'https://api.carmd.com/v3.0/maint';
@@ -29,23 +28,17 @@ function submitVinMileageInfo() {
         event.preventDefault();
         const vin = $('#vinForm #enterVin').val();
         const milesDriven = $('#vinForm #enterMileage').val();
-        //console.log(vin, milesDriven);
-
         getCarInfo(vin, milesDriven);
         getCarMaintenance(vin, milesDriven);
         $('.second').removeClass('hidden');
     });
 }
+
 //gets general car information
 function getCarInfo(vin, milesDriven) {
-    /* const params = {
-         vinNum: vin,
-     };*/
     const genCarInfoQuery = `${vin}?format=json`;
-    //console.log(genCarInfoQuery);
 
     const urlGenCar = genInfoBaseUrl + '/' + genCarInfoQuery;
-    //console.log(urlGenCar);
 
     fetch(urlGenCar)
         .then(response => {
@@ -64,14 +57,12 @@ function getCarInfo(vin, milesDriven) {
 }
 
 //displays general car information
-//fix - display a problem if wrong vin entered.
 function displayGenCar(responseJson, milesDriven) {
     console.log('displayGenCar is working!!');
 
     const results = responseJson.Results;
-    //console.log(results);
 
-    //filter out the desired data
+//filter out the desired data
     const errorVariable = function (results) {
         return results.Variable === 'Error Code';
     };
@@ -83,32 +74,28 @@ function displayGenCar(responseJson, milesDriven) {
         return results.Variable === 'Manufacturer Name';
     };
     const manufacturer = results.filter(manuVariable)[0];
-    //console.log(manufacturer.Value, 'manufacturer');
 
     const makVariable = function (results) {
         return results.Variable === 'Make';
     };
     const make = results.filter(makVariable)[0];
-    //console.log(make.Value, 'make');
 
     const modVariable = function (results) {
         return results.Variable === 'Model';
     };
     const model = results.filter(modVariable)[0];
-    //console.log(model.Value, 'model');
 
     const yrVariable = function (results) {
         return results.Variable === 'Model Year';
     };
     const year = results.filter(yrVariable)[0];
-    //console.log(year.Value, 'year');
-    //console.log(milesDriven, 'mileage');
 
     if (errorCode.ValueId === '0') {
         //Remove empty general info
         $('section .errorDisplay').empty();
         $('section .generalInfo').addClass('hidden');
         $('section .generalInfo').removeClass('grid2');
+
         //Clear any previous info in display gen info 
         $('section .displayGenInfo').empty();
 
@@ -118,19 +105,18 @@ function displayGenCar(responseJson, milesDriven) {
         $('.displayGenInfo').removeClass('hidden');
 
         $('section .displayGenInfo').append(`
-    <div class="a a1"><strong>Manufacturer</strong>:</div> 
-    <div class="b b1"> ${manufacturer.Value}</div>
-    <div class="a a2"><strong>Make</strong>:</div> 
-    <div class="b b2"> ${make.Value}</div>
-    <div class="a a3"><strong>Model</strong>:</div> 
-    <div class="b b3"> ${model.Value}</div>
-    <div class="a a4"><strong>Year</strong>:</div> 
-    <div class="b b4"> ${year.Value}</div>
-    <div class="a a5"> <strong>Mileage</strong>:</div> 
-    <div class="b b5"> ${milesDriven}</div>`);
-
+            <div class="a a1"><strong>Manufacturer</strong>:</div> 
+            <div class="b b1"> ${manufacturer.Value}</div>
+            <div class="a a2"><strong>Make</strong>:</div> 
+            <div class="b b2"> ${make.Value}</div>
+            <div class="a a3"><strong>Model</strong>:</div> 
+            <div class="b b3"> ${model.Value}</div>
+            <div class="a a4"><strong>Year</strong>:</div> 
+            <div class="b b4"> ${year.Value}</div>
+            <div class="a a5"> <strong>Mileage</strong>:</div> 
+            <div class="b b5"> ${milesDriven}</div>`);
     } else {
-    //hide displayGenInfo- if it is showing
+        //hide displayGenInfo- if it is showing
         $('section .displayGenInfo').empty();
         $('section .displayGenInfo').removeClass('grid3');
         $('.displayGenInfo').addClass('hidden');
@@ -145,38 +131,10 @@ function displayGenCar(responseJson, milesDriven) {
 
         <p class="errorMobile"><span><strong> It seems that something is not right...</strong></span><br>
         Double check that your VIN is correct.  yourCARe can only report on vehicles with a 17 character VIN.</p>`);
-
-    
     }
-    /*
-    //Remove empty general info
-    $('section .generalInfo').addClass('hidden');
-    $('section .generalInfo').removeClass('grid2');
-    //Clear any previous info in display gen info 
-    $('section .displayGenInfo').empty();
-
-    //add grid2 to display gen info
-    $('section .displayGenInfo').addClass('grid3');
-    //display gen info
-    $('.displayGenInfo').removeClass('hidden');
-
-    $('section .displayGenInfo').append(`
-    <div class="a a1"><strong>Manufacturer</strong>:</div> 
-    <div class="b b1"> ${manufacturer.Value}</div>
-    <div class="a a2"><strong>Make</strong>:</div> 
-    <div class="b b2"> ${make.Value}</div>
-    <div class="a a3"><strong>Model</strong>:</div> 
-    <div class="b b3"> ${model.Value}</div>
-    <div class="a a4"><strong>Year</strong>:</div> 
-    <div class="b b4"> ${year.Value}</div>
-    <div class="a a5"> <strong>Mileage</strong>:</div> 
-    <div class="b b5"> ${milesDriven}</div>`);
-    */
 }
 
-//data display from response.js
-//console.log(returned.data[0].desc);
-
+//gets Car maintenance information
 function getCarMaintenance(vin, milesDriven) {
 
     const headers = {
@@ -184,7 +142,6 @@ function getCarMaintenance(vin, milesDriven) {
         'authorization': 'Basic YTExYTM0MDYtM2Q4My00ZGM0LWI4ZjktMzc2NjIwNjBmMzEx',
         'partner-token': 'c59e765670124519b76cb7f456879fa5'
     }
-    //console.log(headers);
 
     const myInit = {
         method: 'GET',
@@ -194,31 +151,27 @@ function getCarMaintenance(vin, milesDriven) {
     const maintInfoQuery = `vin=${vin}&mileage=${milesDriven}`;
 
     const urlMaint = forNonProductionHelp + maintenanceBaseURL + '?' + maintInfoQuery;
-    
-        fetch(urlMaint, myInit)
-            .then(response => {
-                if (response.ok) {
 
-                    return response.json();
-                }
-                //console.log(response);
-                throw new Error(response);
-            })
-            .then(responseJson => {
-                //passes responseJson to cleanOutData function
-                //Calls cleanOutData function
-                cleanOutData(responseJson);
-                //console.log(responseJson.message.message, '!!!!!');
-            })
-            .catch(err => {
-                console.log('error', err)
-            });
+    fetch(urlMaint, myInit)
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            }
+            throw new Error(response);
+        })
+        .then(responseJson => {
+            //passes responseJson to cleanOutData function
+            //Calls cleanOutData function
+            cleanOutData(responseJson);
+        })
+        .catch(err => {
+            console.log('error', err)
+        });
 }
 
 function cleanOutData(responseJson) {
-    const data =responseJson.data; //data in json
-    //const data = returned.data
-    
+
+    const data = responseJson.data; //data in json
     console.log(data);
     const condensed = [];
     console.log(data[0].desc);
@@ -228,7 +181,7 @@ function cleanOutData(responseJson) {
     //but not those with duplicate desc
     //push mileage value from duplicate desc into object in condensed
     for (let obj in data) {
-        let duplicate = false; // flag: add new obj or note?
+        let duplicate = false;
 
         for (let i = 0; i < condensed.length; i++) {
             if (condensed[i].hasOwnProperty('desc')) {
@@ -249,7 +202,6 @@ function cleanOutData(responseJson) {
 }
 
 function displayCarMaintenance(condensed) {
-
     $('.displayMaintenance').empty();
     console.log(condensed);
     for (let i = 0; i < condensed.length; i++) {
@@ -261,7 +213,7 @@ function displayCarMaintenance(condensed) {
             <td class="align" id="displayMileages${i}"></td>
             </tr>`);
         for (let j = 0; j < mileage.length; j++) {
-            $(`#displayMileages${i}`).append(`<td class="miles-cell">${mileage[j]}</td>`);
+            $(`#displayMileages${i}`).append(`${mileage[j]}   `);
             console.log(condensed[i].mileage);
         }
     };
